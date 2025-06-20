166980d1696f9e6fb5b0402808107e38c80c95ff
Tarun Thakur <tarunthakur285@gmail.com>
2025-06-20 15:27:51 +0530

    Add CORS middleware and update dependencies for improved security and functionality

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const authRoutes = require('./src/routes/authRoutes');
-const cookieParser = require('cookie-parser');
+const express = require('express'); // Include the express module
+const cookieParser = require('cookie-parser'); // Include the cookie-parser module
+const cors = require('cors'); // Include the cors module
+const authRoutes = require('./src/routes/authRoutes'); // Include your auth routes
 
-const app = express();
+const app = express(); // Instantiate express app
 
+// Middleware to parse JSON and cookies
 app.use(express.json());
-app.use(cookieParser()); 
-app.use('/auth', authRoutes); // 
+app.use(cookieParser());
 
-const PORt = 5001;
+// CORS configuration
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+};
 
-app.listen(5001, (error) => {
-    if (error) {
-        console.log("Error in server setup", error);
-    } else {
-        console.log("Server is running on port", PORt);
-    }
-});
+app.use(cors(corsOptions));
+
+// Use the auth routes
+app.use('/auth', authRoutes);
+
+// Set up the server to listen
+const PORT = 5001;
